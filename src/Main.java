10fe197575d33fd2f0991d83734ead8a14014b41
delarbrm <delarbrm@PC-B103-01.iut-valence.fr>
2016-02-19 14:29:16 +0100

    party and main create

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+	  Mastermind mastermindparty = new Mastermind();//start new Mastermindparty.
+	  mastermindparty.start();
 
 	}
 
--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,5 +1,11 @@
 
-public class Mastermind
+public class Mastermind //Mastermindparty class
 {
+
+	public void start() //allow to start a game
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 }
