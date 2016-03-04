4252b0dd12a1e8bee2f92789de491fdb00a5510d
delarbrm <delarbrm@PC-B103-08.iut-valence.fr>
2016-03-04 16:30:01 +0100

    creation mastermind base

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 
 	public static void main(String[] args)
 	{
-	  Mastermind mastermindparty = new Mastermind();//start new Mastermindparty.
+	  Mastermind mastermindparty = new Mastermind();/*start new Mastermindparty.*/
 	  mastermindparty.start();
 
 	}
--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,8 +1,18 @@
 
-public class Mastermind //Mastermindparty class
+public class Mastermind /* Mastermindparty class */
 {
-
-	public void start() //allow to start a game
+	public Mastermind()
+	{
+	 Plate plateau = new plate();
+	 Ball b1 =new ball();   /* combinaison de l'ia*/
+	 Ball b2 =new ball();   /* combinaison de l'ia*/
+	 Ball b3 =new ball();   /* combinaison de l'ia*/
+	 Ball b4 =new ball();   /* combinaison de l'ia*/ 
+		
+	}
+	
+	
+	public void start() /* allow to start a game */
 	{
 		// TODO Auto-generated method stub
 		
