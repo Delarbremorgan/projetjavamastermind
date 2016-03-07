67370102b3867c456b0996783dafbbf96ee8bc70
delarbrm <delarbrm@PC-B103-01.iut-valence.fr>
2016-03-07 17:56:02 +0100

    modification colors class

--- src/Colors.java
+++ src/Colors.java
@@ -4,39 +4,39 @@ public class Colors
 	/**
 	 * number of color max
 	 */
-	public int maxcolor=6; 
+	public static final int maxcolor=6; 
 	/**
 	 * number of color min
 	 */
-	public int mincolor=0; 
+	public static final int mincolor=0; 
 	/**
 	*distribution of color
 	*/
-	public int blue=0;
+	public static final int blue=0;
 	/**
 	*distribution of color
 	*/
-	public int red=1;
+	public static final int red=1;
 	/**
 	*distribution of color
 	*/
-	public int yellow=2;
+	public static final int yellow=2;
 	/**
 	*distribution of color
 	*/
-	public int pink=3;
+	public static final int pink=3;
 	/**
 	*distribution of color
 	*/
-	public int green=4;
+	public static final int green=4;
 	/**
 	*distribution of color
 	*/
-	public int orange=5;
+	public static final int orange=5;
 	/**
 	*distribution of color
 	*/
-	public int purple=6;
+	public static final int purple=6;
 	/**
 	*color
 	*/
