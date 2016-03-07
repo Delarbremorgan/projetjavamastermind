c86e2010ca441e42e3a5dd987577ab51b7cda9fa
delarbrm <delarbrm@PC-B123-01.iut-valence.fr>
2016-03-07 15:51:38 +0100

    javacode update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
-public class Main
+public class Main 
 {
 
 	public static void main(String[] args)
 	{
-	  Mastermind mastermindparty = new Mastermind();/*start new Mastermindparty.*/
-	  mastermindparty.start();
+	  Mastermind mastermindparty = new Mastermind();/**start new Mastermindparty.*/
+	  mastermindparty.start(); /** party start*/
 
 	}
 
--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,18 +1,18 @@
 
-public class Mastermind /* Mastermindparty class */
+public class Mastermind /** Mastermindparty class */
 {
-	public Mastermind()
+	public Mastermind()/**game can be start*/
 	{
 	 Plate plateau = new plate();
-	 Ball b1 =new ball();   /* combinaison de l'ia*/
-	 Ball b2 =new ball();   /* combinaison de l'ia*/
-	 Ball b3 =new ball();   /* combinaison de l'ia*/
-	 Ball b4 =new ball();   /* combinaison de l'ia*/ 
+	 Ball b1 =new ball();   /** combinaison de l'ia*/
+	 Ball b2 =new ball();   /** combinaison de l'ia*/
+	 Ball b3 =new ball();   /** combinaison de l'ia*/
+	 Ball b4 =new ball();   /** combinaison de l'ia*/ 
 		
 	}
 	
 	
-	public void start() /* allow to start a game */
+	public void start() /** allow to start a game */
 	{
 		// TODO Auto-generated method stub
 		
