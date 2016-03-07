c1d7ca60d5c88de11a10c20d305d6c1aa4b39000
sebastienjean <baz.jean@gmail.com>
2016-03-07 12:07:55 +0100

    Renamed project, added some TODO

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// TODO update README.md (what/who)
+
+// TODO write comment
+public class Main
+{
+	// TODO write comment
+	public static void main(String[] args)
+	{
+		new MastermindGame().play();
+	}
+
+}
